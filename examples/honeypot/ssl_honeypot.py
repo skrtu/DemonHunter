a0402025ce783ffb79a9c2d67b6569a0ba152bd8
skrtu <3kthegiant@gmail.com>
2017-05-14 02:11:44 -0400

    Create ssl honeypot to test SSL

---
+++ examples/honeypot/ssl_honeypot.py
@@ -0,0 +1,31 @@
+import asyncio
+
+from demonhunter import DemonHunter
+from demonhunter.nodes.honeypots.telnet import TelnetHoneypot, MicrosoftTelnet
+from demonhunter.nodes.honeypots.vnc import VNCHoneypot
+from demonhunter.nodes.honeypots import Agent
+
+# from demonhunter.nodes.honeypots.http import HTTPHoneypot
+
+loop = asyncio.get_event_loop()
+hp = DemonHunter(loop)
+
+vnc = VNCHoneypot(interfaces=["0.0.0.0"])
+hp.add_honeypot(vnc)
+
+telnet = TelnetHoneypot(port=23, handler=MicrosoftTelnet, interfaces=["0.0.0.0"])
+hp.add_honeypot(telnet)
+
+agent = Agent(["0.0.0.0"], [telnet, vnc], loop, certificate='selfsigned.cert')
+hp.add_agent(agent)
+
+hp.start()
+
+try:
+    # Run The Loop
+    loop.run_forever()
+except KeyboardInterrupt:
+    hp.stop()
+    print("\nServer Closed")
+
+loop.close()
